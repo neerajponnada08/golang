75d5b4c912676fb2756b34d850ef227fe2c03e1b
Ponnada Neeeraj Vardhan <neeraj.vardhan@indiamart.com>
2022-07-26 20:06:24 +0530

    Addedd API Codes

---
+++ 27myAPI/go.mod
@@ -0,0 +1,5 @@
+module github.com/neerajponnada08/golang/27myAPI
+
+go 1.18
+
+require github.com/gorilla/mux v1.8.0 // indirect
---
+++ 27myAPI/main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+// Model for course - file
+
+type Course struct {
+	CourseId    string  `json:"courseid"`
+	CourseName  string  `json:"coursename"`
+	CoursePrice int     `json:"price"`
+	Author      *Author `json:"author"`
+}
+
+type Author struct {
+	Fullname string `json:"fullname"`
+	Website  string `json:"website"`
+}
+
+// Fake db
+
+var courses []Course
+
+// middleware or helper methods
+
+func (c *Course) IsEmpty() bool {
+	// return c.CourseId == "" && c.CourseName == ""
+	return c.CourseName == ""
+}
+
+func main() {
+
+}
+
+// Controllers - file
+
+// serve home route
+
+func serverHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Home - Welcome to API</h1>"))
+}
+
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get all Courses")
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(courses)
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get One Course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// grab id from request
+	params := mux.Vars(r)
+
+	// loop throught the courses, find matching id and return the response
+
+	for _, course := range courses {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("No Course found with given id")
+	return
+}
+
+func createOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Create One Course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// what if: body is empty
+
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please send some data")
+	}
+
+	// what about - {}
+
+	var course Course
+	_ = json.NewDecoder(r.Body).Decode(&course)
+	if course.IsEmpty() {
+		json.NewEncoder(w).Encode("no data inside")
+		return
+	}
+
+	// generate the unique id, string
+	// append the course to courses
+
+	rand.Seed(time.Now().UnixNano())
+	course.CourseId = strconv.Itoa(rand.Intn(100))
+	courses = append(courses, course)
+	json.NewEncoder(w).Encode(course)
+	return
+
+}
+
+func updateOneCourse (w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Update One Course")
+	w.Header().Set("Content-Type", "application/json")
+
+	//first - grab id from req
+	params := mux.Vars(r)
+
+	//loop, id, remove, add with my ID
+
+	for index, course := range courses {
+		if course.CourseId == params["id"] {
+			courses = append(courses[:index],courses[index+1:]... )
+			var course Course
+			_ = json.NewDecoder(r.Body).Decode(&course)
+			course.CourseId = params["id"]
+			courses = append(courses, course)
+			json.NewEncoder(w).Encode(course)
+			return 
+		}
+	}
+}
